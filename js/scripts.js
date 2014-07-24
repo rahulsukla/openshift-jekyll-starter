1aeb72c51a77b329b57a54cc086d13db739865e6
rahul <rahulshukla111@gmail.com>
2014-07-24 15:16:50 +0530

    Iframe Fix added

--- js/scripts.js
+++ js/scripts.js
@@ -102,3 +102,25 @@ $(document).ready(function(){
         });
     }
 });
+
+/*****  Iframe JS Fix *******/
+function adjustIframes()
+{
+  $('iframe').each(function(){
+    var
+    $this       = $(this),
+    proportion  = $this.data( 'proportion' ),
+    w           = $this.attr('width'),
+    actual_w    = $this.width();
+    if ( ! proportion )
+    {
+        proportion = $this.attr('height') / w;
+        $this.data( 'proportion', proportion );
+    }
+    if ( actual_w != w )
+    {
+        $this.css( 'height', Math.round( actual_w * proportion ) + 'px' );
+    }
+  });
+}
+$(window).on('resize load',adjustIframes);
